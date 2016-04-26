6bcf17890d49a6e70b423e54f3ec503436f6121c
Jason Carlson <Jason Carlson>
2016-04-26 08:02:44 -0600

    Added Board module shell

---
+++ Board.hs
@@ -0,0 +1,34 @@
+module Board(
+	mkBoard,
+	mkPlayer,
+	mkOpponent,
+	size,
+	row,
+	column,
+	mark,
+	isEmpty,
+	isMarked,
+	isMarkedBy,
+	marker,
+	isFull,
+	isWonBy,
+	isDraw,
+	isGameOver,
+	boardToStr) where
+
+	mkBoard = False
+	mkPlayer = False
+	mkOpponent = False
+	size = False
+	row = False
+	column = False
+	mark = False
+	isEmpty = False
+	isMarked = False
+	isMarkedBy = False
+	marker = False
+	isFull = False
+	isWonBy = False
+	isDraw = False
+	isGameOver = False
+	boardToStr = False
