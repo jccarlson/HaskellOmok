7d13538e60bb073100b7b33dd7a92c6d7c89c6f9
Jason Carlson <Jason Carlson>
2016-04-27 19:58:07 -0600

    Board.hs module Complete with comments

--- Board.hs
+++ Board.hs
@@ -1,3 +1,10 @@
+-----------------------------------------------------------------------------
+--
+-- module       :  HaskellOmok.Board
+-- Authors		:  Jason Carlson, Aldo Perez
+-----------------------------------------------------------------------------
+
+
 module Board(
 mkBoard,
 mkPlayer,
@@ -16,6 +23,12 @@ isDraw,
 isGameOver,
 boardToStr) where
 
+{-|
+	 Return an empty nxn board, where n is a positive number. A 1-based 
+     pair of indices (x,y) will be used to access a specific place of
+     the board, where x and y are column and row indices, respectively.
+     However, it's up to you how to represent an omok board concretely.
+-}
 mkBoard :: Int -> [Char]
 mkBoard n   |   n <= 0      =   []
             |   otherwise   =   getEmptyListOfLength(n*n)
@@ -24,12 +37,28 @@ getEmptyListOfLength :: Int -> [Char]
 getEmptyListOfLength 1 = [' ']
 getEmptyListOfLength n = (' ':getEmptyListOfLength(n-1))
 
+{-|
+	Create and return the first player.
+-}
+mkPlayer :: Char
 mkPlayer = 'X'
+
+{-|
+	Create and return the second player, i.e., the opponent.
+-}
+mkOpponent :: Char
 mkOpponent = 'O'
 
+{-|
+	Return the size of a board bd, n for an nxn board.
+-}
 size :: [Char] -> Int
 size bd = (floor . sqrt . fromIntegral . length) bd
 
+{-|
+	Return a row y of a board bd, where y is a 1-based index. It returns
+    a list of size n, where n is the size of bd.
+-}
 row :: Int -> [Char] -> [Char]
 row y bd   |   y < 1           =   []
            |   y > (size bd)   =   []
@@ -41,6 +70,10 @@ getSubList 0     0    (h:l) = [h]
 getSubList 0     end  (h:l) = (h:(getSubList 0 (end - 1) l))
 getSubList start end  (h:l) = getSubList (start - 1) (end - 1) l
 
+{-|
+	Return a column x of a board bd, where x is a 1-based index. It
+    returns a list of size n, where n is the size of bd.
+-}
 column :: Int -> [Char] -> [Char]
 column _ []                       =   []
 column x bd   |   x < 1           =   []
@@ -51,31 +84,56 @@ getEveryNthElem :: Int -> [a] -> [a]
 getEveryNthElem _ [] = []
 getEveryNthElem n l  = ((head l):(getEveryNthElem n (drop n l)))
 
-
+{-|
+	Mark a place (x,y) in a board bd by a player p, where x and y 
+    are 1-based column and row indices. The specified place is assumed
+    to be empty (see below).
+-}
 mark :: Int -> Int -> [Char] -> Char -> [Char]
 mark x y bd pl = firstHalf ++ [pl] ++ lastHalf
-               where (firstHalf, (_:lastHalf)) = splitAt ((((y - 1) * (size bd)) + (x - 1))) bd  
+               where (firstHalf, (_:lastHalf)) = splitAt ((((y - 1) * (size bd)) + (x - 1))) bd 
 
+{-|
+	Is a place (x,y) of a board bd unmarked or a stone not placed? 
+    The x and y are 1-based column and row indices.
+-}
 isEmpty :: Int -> Int -> [Char] -> Bool
 isEmpty x y bd = ((marker x y bd) == ' ')
-
+{-|
+	Does a place (x,y) of a board bd have a stone placed? The x and y 
+    are 1-based column and row indices.
+-}
 isMarked :: Int -> Int -> [Char] -> Bool
 isMarked x y bd = not (isEmpty x y bd)
 
+{-|
+	Does a place (x,y) of a board bd have a stone placed by a player p?
+    The x and y are 1-based column and row indices.
+-}
 isMarkedBy :: Int -> Int -> [Char] -> Char -> Bool
 isMarkedBy x y bd pl = ((marker x y bd) == pl)             
 
+{-|
+	Return the player of the stone placed on a place (x,y) of a board 
+    bd. The x and y are 1-based column and row indices.
+-}
 marker :: Int -> Int -> [Char] -> Char
 marker x y bd   |  (((y - 1) * (size bd)) + (x - 1)) < 0            = ' '
                 |  (((y - 1) * (size bd)) + (x - 1)) >= (length bd) = ' ' 
                 |  otherwise                                        = head lastHalf
                 where lastHalf = drop ((((y - 1) * (size bd)) + (x - 1))) bd
 
+{-|
+	Are all places of board bd marked?
+-}
 isFull :: [Char] -> Bool
 isFull []        = True
 isFull (' ':rem) = False 
 isFull (_:rem)   = isFull rem
 
+{-|
+	Is the game played on a board bd won by a player p?
+-}
 isWonBy :: [Char] -> Char -> Bool
 isWonBy bd pl = checkRemainingPlaces bd pl ((size bd) * (size bd))
 
@@ -89,12 +147,23 @@ checkSequence :: [Char] -> Char -> Int -> Int -> Int -> Int -> Int -> Bool
 checkSequence bd pl x y dx dy 0   = True
 checkSequence bd pl x y dx dy rem = (isMarkedBy x y bd pl) && (checkSequence bd pl (x + dx) (y + dy) dx dy (rem - 1))
 
+{-|
+	Is the game played on a board bd ended in a draw?
+-}
 isDraw :: [Char] -> Bool
 isDraw bd = (isFull bd) && (not (isWonBy bd 'X')) && (not (isWonBy bd 'O'))
 
+{-|
+	Is the game played on a board bd over?
+-}
 isGameOver :: [Char] -> Bool
 isGameOver bd = (isDraw bd) || (isWonBy bd 'X') || (isWonBy bd 'O')
 
+{-|
+	Return a string representation of a board bd. It is a higher-order
+    function, and playerToChar is a function that converts a player 
+    to a character representation, e.g., 'O' and 'X'.
+-}
 boardToStr :: (Char -> Char) -> [Char] -> [Char]
 boardToStr playerToChar bd = allRowsToStrings playerToChar bd (size bd)
 
@@ -105,4 +174,3 @@ allRowsToStrings playerToChar bd currRow = (allRowsToStrings playerToChar bd (cu
 rowToString :: (Char -> Char) -> [Char] -> [Char]
 rowToString _            []      = []
 rowToString playerToChar (h:rem) = [(playerToChar h)] ++ [' '] ++ (rowToString playerToChar rem)
- 
